2fff6d507e4d93ef3970f5549e8a7f2b10fe227e
Egor Loktev <loktev@lok-tech.com>
2024-06-21 16:57:10 +0300

    Update settings

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.project_name, 
+            #   openapi_url=f"{settings.api_v1_str}/openapi.json", 
+              version=settings.api_version,
+              contact={"name": "Egor Loktev", "url": "https://t.me/eloktev"})
--- app/app/core/config.py
+++ app/app/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
+    project_name: str
+    api_version: str
 
     class Config:
         env_file = ".env"
